0451b9886c34b3418735f078f3b39ec05ef59601
wenxuan liu <30834783+PROTONlwx@users.noreply.github.com>
2022-04-01 21:33:04 -0700

    remove scripting permission

--- action.js
+++ action.js
@@ -1,16 +1,6 @@
-// let a = new IdClick("addbtn", null, null);
-// let b = new IdClick("addbtn", null, null);
-// let c = new IdClick("addbtn", null, null);
-// a.next = b;
-// b.prev = a;
-// b.next = c;
-// c.prev = b;
-// a.executeAction();
-
-// console.log(a.getObjRepresentation());
-// console.log(b.getObjRepresentation());
-// console.log(c.getObjRepresentation());
-
+// Listen for the popup javascript message
+// Respond to request = actions
+// Make an action chain and start execute action.
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (message.request.valueOf() == "actions".valueOf()) {
     let c = new Chain(message.actions);
